f6208149137bef10f5210673b7eb67ef219b5aae
Carlos <carlosgutz25@gmail.com>
2017-06-23 19:42:52 -0700

    more..

--- Startup.cs
+++ Startup.cs
@@ -28,10 +28,13 @@ namespace FirstBeltExam{
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole();
-            app.UseDeveloperExceptionPage();
+            if( env.IsDevelopment() )
+            {
+                loggerFactory.AddConsole();
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
             app.UseSession();
             app.UseMvc();
